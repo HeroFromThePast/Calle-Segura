bcc82d160a588620645fd30ad96444b5d10ed80d
Popeton <93623436+Popeton@users.noreply.github.com>
2022-05-02 00:23:33 -0500

    botones y arreglos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-tfyd6rjkdr
+window.onload = init;
+
+function irA(enlace){
+	if(enlace == 'Perfil.html'){
+        location.href = enlace;
+	} 
+    
+         if(enlace == 'main.html'){
+        location.href = enlace;
+	} 
+
+        if(enlace == 'barraBusqueda.html'){
+                location.href = enlace;
+        } 
+
+        if(enlace == 'generarRuta1.html'){
+                location.href = enlace;
+        } 
+
+        if(enlace == 'generarRuta2.html'){
+                location.href = enlace;
+        } 
+
+        if(enlace == 'vistaFavoritos.html'){
+                location.href = enlace;
+        } 
+
+        
+         
+    
+}
